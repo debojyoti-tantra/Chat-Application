ed6e785cf7f2c3da0c7abd19ec282fbd4e31ef67
Debojyoti Tantra <tantradebojyoti@gmail.com>
2025-01-08 23:41:08 +0530

    for deploying to vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'UNRESOLVED_IMPORT') return;
+        warn(warning); // Let other warnings be handled normally
+      }
+    }
+  }
 })
