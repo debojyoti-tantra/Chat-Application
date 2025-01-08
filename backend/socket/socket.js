a7912fa1ac63e02eeda0458e16c183dafd3df041
Debojyoti Tantra <tantradebojyoti@gmail.com>
2025-01-08 22:00:52 +0530

    Add process.env in socket

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -2,8 +2,8 @@ import {Server} from "socket.io";
 import http from "http";
 import express from "express";
 
-// import dotenv from "dotenv";
-// dotenv.config({});
+import dotenv from "dotenv";
+dotenv.config({}); 
 
 const app = express();
 
@@ -11,8 +11,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
    cors: {
-      origin: ["http://localhost:5173"],
-      // origin: process.env.FRONTEND_URI,
+      // origin: ["http://localhost:5173"],
+      origin: process.env.FRONTEND_URI,
       methods: ['GET', 'POST'],
       
    },
